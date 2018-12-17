1fcca6b9fd7cc5c07fa4f710490d3720cbd21b9d
Cold Zebra <zachdbray@gmail.com>
2018-12-16 20:33:11 -0600

    12-16-18 8pm stopping spot

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,52 +10,58 @@ var blueNumber= Math.floor(Math.random()*11+1);
 var purpleNumber= Math.floor(Math.random()*11+1);
 var randomTarget= Math.floor(Math.random()*102+18);
 
-
-
 //On Clicks for Each Button
 //Includes win/loss check
 $("#green").on("click", function(){
-    alert("Green!");
-    currentTotal =+ greenNumber;
-    $("#currentTotal").text(" " + currentTotal);
+    alert(currentTotal);
+    currentTotal += greenNumber;
+    $("#currentTotal").text(currentTotal);
     alert(greenNumber);
     alert(currentTotal);
 })
 
 $("#blue").on("click", function(){
-    alert("Blue!");
-    currentTotal =+ blueNumber;
-    $("#currentTotal").text(" " + currentTotal);
+    currentTotal += blueNumber;
+    $("#currentTotal").text(currentTotal);
     alert(greenNumber);
     alert(currentTotal);
 })
 
 $("#red").on("click", function(){
-    alert("Red!");
-    $("#currentTotal").text(" " + currentTotal);
+    currentTotal += redNumber;
+    $("#currentTotal").text(currentTotal);
     alert(redNumber);
     alert(currentTotal);
 })
 
 $("#purple").on("click", function(){
-    alert("Purple!");
-    $("#currentTotal").text(" " + currentTotal);
+    currentTotal += purpleNumber;
+    $("#currentTotal").text(currentTotal);
     alert(purpleNumber);
     alert(currentTotal);
 })
-
 
 //Displays number of wins/losses/goal with span ids in html
 $("#numWins").text(": " + wins);
 $("#numLosses").text(": " + losses);
 $(".randomTarget").text(randomTarget);
 
-
 //runs Game after win or loss and at webpage open
-// function runGame() {
-//     currentTotal=0;
-// }
+function runGame() {
+    randomTarget= Math.floor(Math.random()*102+18);
+}
+
 //Loss and win functions
+
+// function check (){
+//     if (currentTotal === randomTarget){
+//         gameWin();
+//     }
+//     else if (currentTotal > randomTarget){
+//         gameLoss;
+//     }
+// }
+
 function gameLoss(){
     alert("Ah no luck today. Ganon lives to fight another day.")
     losses++;
@@ -65,8 +71,6 @@ function gameLoss(){
 function gameWin(){
     alert("You're the hero I've been looking for! Congratulations, the Pictograph is yours!")
     wins++;
-    rungGame();
+    runGame();
 }
-
-
 });
