3dd05ffd7487355081515b8dddaeca129610bfe6
Cold Zebra <zachdbray@gmail.com>
2018-12-18 19:15:03 -0600

    fixes counters adds notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,43 +2,59 @@ $(document).ready(function(){
     
 // Needed Variables
 var wins = 0;
-var losses = 0;
-var currentTotal;
+var losses =0;
+var currentTotal =0;
 var redNumber= Math.floor(Math.random()*11+1);
 var greenNumber= Math.floor(Math.random()*11+1);
 var blueNumber= Math.floor(Math.random()*11+1);
 var purpleNumber= Math.floor(Math.random()*11+1);
 var randomTarget= Math.floor(Math.random()*102+18);
 
+
+//Reset Function after win or loss
+function reset(){
+  currentTotal = 0;
+  randomTarget= Math.floor(Math.random()*102+18);
+  $(".randomTarget").text(randomTarget);
+  redNumber= Math.floor(Math.random()*11+1); 
+  greenNumber= Math.floor(Math.random()*11+1);
+  blueNumber= Math.floor(Math.random()*11+1);
+  purpleNumber= Math.floor(Math.random()*11+1);
+}
+
 //On Clicks for Each Button
-//Includes win/loss check
+//Checks each time for win/loss
 $("#green").on("click", function(){
-    alert(currentTotal);
-    currentTotal += greenNumber;
-    $("#currentTotal").text(currentTotal);
-    alert(greenNumber);
-    alert(currentTotal);
+  currentTotal += greenNumber;
+  $("#currentTotal").text(": "+ currentTotal);
+  console.log("red"+greenNumber);
+  console.log("current"+currentTotal);
+  check();
+
 })
 
 $("#blue").on("click", function(){
-    currentTotal += blueNumber;
-    $("#currentTotal").text(currentTotal);
-    alert(greenNumber);
-    alert(currentTotal);
+  currentTotal += blueNumber;
+  $("#currentTotal").text(": "+ currentTotal);
+  console.log("blue"+greenNumber);
+  console.log("current"+currentTotal);
+  check();
 })
 
 $("#red").on("click", function(){
-    currentTotal += redNumber;
-    $("#currentTotal").text(currentTotal);
-    alert(redNumber);
-    alert(currentTotal);
+  currentTotal += redNumber;
+  $("#currentTotal").text(": "+ currentTotal);
+  console.log("red"+redNumber);
+  console.log("current"+currentTotal);
+  check();
 })
 
 $("#purple").on("click", function(){
-    currentTotal += purpleNumber;
-    $("#currentTotal").text(currentTotal);
-    alert(purpleNumber);
-    alert(currentTotal);
+  currentTotal += purpleNumber;
+  $("#currentTotal").text(": "+ currentTotal);
+  console.log("purple"+purpleNumber);
+  console.log("current"+currentTotal);
+  check();
 })
 
 //Displays number of wins/losses/goal with span ids in html
@@ -46,31 +62,20 @@ $("#numWins").text(": " + wins);
 $("#numLosses").text(": " + losses);
 $(".randomTarget").text(randomTarget);
 
-//runs Game after win or loss and at webpage open
-function runGame() {
-    randomTarget= Math.floor(Math.random()*102+18);
-}
-
-//Loss and win functions
-
-// function check (){
-//     if (currentTotal === randomTarget){
-//         gameWin();
-//     }
-//     else if (currentTotal > randomTarget){
-//         gameLoss;
-//     }
-// }
-
-function gameLoss(){
-    alert("Ah no luck today. Ganon lives to fight another day.")
-    losses++;
-    runGame();
-}
-
-function gameWin(){
+//Loss and win function
+function check (){
+  if (currentTotal === randomTarget){
     alert("You're the hero I've been looking for! Congratulations, the Pictograph is yours!")
     wins++;
-    runGame();
+    $("#numWins").text(": " + wins);
+    reset();
+  }
+
+    else if (currentTotal > randomTarget){
+      alert("Ah no luck today. Ganon lives to fight another day.")
+      losses++;
+      $("#numLosses").text(": " + losses);
+      reset();
+    }
 }
 });
